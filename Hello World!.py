d6b9ffc20019e792c015d2feb0ef7c8bdb301ca6
Johan <johan.snyman@orange.com>
2019-09-26 14:03:09 +1000

    Initial file

---
+++ Hello World!.py
@@ -0,0 +1,13 @@
+# This program says hello and asks for my name.
+
+print('Hello World!')
+# Ask for their name
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+# Ask for their age
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
