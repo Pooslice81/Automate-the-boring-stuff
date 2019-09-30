312a169028cb9a2b976fd58074bd381f636182a8
Johan <johan.snyman@orange.com>
2019-09-30 10:14:19 +1000

    catching errors that occur in function calls in a try block

--- zeroDivide.py
+++ zeroDivide.py
@@ -9,3 +9,14 @@ print(spam(2))
 print(spam(12))
 print(spam(0))
 print(spam(1))
+
+def spam(dividyBy):
+    return 42 / dividyBy
+
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+except ZeroDivisionError:
+    print('Error: Invalid argument. Division by zero is not possible')
